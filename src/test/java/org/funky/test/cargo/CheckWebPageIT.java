581dd5f74434f6e60e187b4a4dfc29a4bb3aad3d
Pierre Bertrand <funkygono@gmail.com>
2014-02-16 23:00:18 +0100

    Add JBoss 6 + Enhance test.

--- src/test/java/org/funky/test/cargo/CheckWebPageIT.java
+++ src/test/java/org/funky/test/cargo/CheckWebPageIT.java
@@ -1,10 +1,16 @@
 package org.funky.test.cargo;
 
+import com.google.common.io.CharStreams;
 import org.junit.Test;
 
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
 
 /**
  * Integration test that checks the web application deployed with Cargo.
@@ -19,8 +25,13 @@ public class CheckWebPageIT {
         URL url = new URL("http://localhost:8080/cargo-test/index.html");
         HttpURLConnection connection = (HttpURLConnection) url.openConnection();
         connection.setRequestMethod("GET");
-        int responseCode = connection.getResponseCode();
-        System.out.println("Response code: " + responseCode);
+        assertThat(connection.getResponseCode(), is(200));
+        assertThat(getResponseAsString(connection),
+                containsString("Welcome to the cargo test application"));
+    }
+
+    private String getResponseAsString(HttpURLConnection connection) throws IOException {
+        return CharStreams.toString(new InputStreamReader(connection.getInputStream()));
     }
 
 }
